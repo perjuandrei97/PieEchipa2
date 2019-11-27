82267f1a7c26bdbac7b96204161f823fa653b603
floringh97 <floringh97@gmail.com>
2019-11-27 14:39:22 +0200

    Rezultate pe materii

--- OlimpiadaNationala.php
+++ OlimpiadaNationala.php
@@ -8,7 +8,7 @@ try {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       // use exec() because no results are returned
     $sql = "select cod,nume_candidati,prenume_candidati,clasa_candidati,scoala_candidati,judet_candidati,localitate_candidati from candidati";
-    $query="select * from rezultate";
+    $query="select cod, nume_candidati, prenume_candidati, clasa_candidati, scoala_candidati, localitate_candidati, judet_candidati, proba1, proba2, proba3, materie.cod_materie as cod_mat, materie.denumire_materie as den_mat from rezultate, materie where rezultate.cod_materie=materie.cod_materie order by cod_mat, nume_candidati";
     }catch(PDOException $e)
     {
     echo $sql . "<br>" . $e->getMessage();
@@ -477,6 +477,7 @@ function myFunction() {
      <th>Proba 1</th>
       <th>Proba 2</th>
        <th>Proba 3</th>
+	   <th>Materie</th>
   
   <?php
 
@@ -492,7 +493,7 @@ function myFunction() {
     							echo "<td>".$row["proba1"]."</td>";
     								echo "<td>".$row["proba2"]."</td>";
     									echo "<td>".$row["proba3"]."</td>";
-    						
+										echo "<td>".$row["den_mat"]."</td>";
     						echo "</tr>";
       
 }
