43fe4272b2c28a79a851ce0377a0ca28d6bd0c45
lstoianx <pato_lucyan@yahoo.com>
2019-11-30 17:06:38 +0200

    prima pagina design

--- Welcome.php
+++ Welcome.php
@@ -9,7 +9,7 @@ try {
       // use exec() because no results are returned
     $sql = "select cod,nume_candidati,prenume_candidati,clasa_candidati,scoala_candidati,judet_candidati,localitate_candidati from candidati";
     $query="select cod, nume_candidati, prenume_candidati, clasa_candidati, scoala_candidati, localitate_candidati, judet_candidati, proba1, proba2, proba3, materie.cod_materie as cod_mat, materie.denumire_materie as den_mat from rezultate, materie where rezultate.cod_materie=materie.cod_materie order by cod_mat, nume_candidati";
-    }catch(PDOException $e)
+  }catch(PDOException $e)
     {
     echo $sql . "<br>" . $e->getMessage();
     $conn = null;
@@ -17,11 +17,35 @@ try {
 ?>
 
 <html>
- <head>
- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-  <title>Olimpiada nationala la matematica</title>
-  &nbsp&nbsp&nbsp<center><body onload="startTime()">
-<div id="txt"></div></center>
+	<head>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	</head>
+	<meta charset ="UTF-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1 ">
+	
+<style>
+.fa {
+  padding: 20px;
+  font-size: 20px;
+  width:50px;
+  height:50px;
+  text-align: center;
+  text-decoration: none;
+  margin: 5px 2px;
+  border-radius: 50%;
+}
+
+.fa:hover {
+    opacity: 0.7;
+}
+</style>
+</head>
+<body style="background-color:#60809f;" onload="startTime()">
+	<center>
+		<div class="container" id="txt" style="background-color:#e6e6e6; width:900px;">
+		</div>
+	</center>
 <script>
 function startTime() {
   var today = new Date();
@@ -38,138 +62,94 @@ function checkTime(i) {
   if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
   return i;
 }
-</script></head>
-  <meta charset ="UTF-8">
-  <meta name="viewport" content="width=device-width,initial-scale=1 ">
-<style>
-table {
-  font-family: arial, sans-serif;
-  border-collapse: collapse;
-  width: 70%;
-  text-align: center;
-}
-
-td, th {
-  border: 1px solid #dddddd;
-  text-align: left;
-  padding: 8px;
-}
-
-tr:nth-child(even) {
-  background-color: #dddddd;
-}
-
-body {font-family: Arial;
-height :100%;
-margin: 0;}
-
-/* Style the tab */
-.tab {
-  overflow: hidden;
-  border: 1px solid #ccc;
-  background-color: #f1f1f1;
-  display: inline-block;
-  margin-top: 20px;
+</script>
  
-}
+ <div class="container" style="background-color:#e6e6e6; height:250px; width:900px; text-align:center;">
+	<br>
+	<h1><center><i>Olimpiada națională</i></center></h1>
+<br><br>
+<center> 
+		<div class="g-heading-v9 text-center g-mb-30">
+			<a class="logo inline">
+				<div class="text-center" style="opacity:.8;">
+					<img src="imagini/1.png" alt="" style="max-height: 100px; max-width:90%;">
+					<img src="imagini/2.png" alt="" style="max-height: 100px; max-width:90%;">
+					<img src="imagini/3.png" alt="" style="max-height: 100px; max-width:90%;">
+					<img src="imagini/4.png" alt="" style="max-height: 100px; max-width:90%;">
+				</div>	
+			</a>
+		</div>
+	</center>
+</div>
+<br>
 
-/* Style the buttons inside the tab */
-.tab button {
-  background-color: inherit;
-  float: left;
-  border: none;
-  outline: none;
-  cursor: pointer;
-  padding: 14px 16px;
-  transition: 0.3s;
-  font-size: 17px;
-
-}
-
-/* Change background color of buttons on hover */
-.tab button:hover {
-  background-color: #ddd;
-}
-
-/* Create an active/current tablink class */
-.tab button.active {
-  background-color: #ccc;
-}
-
-/* Style the tab content */
-.tabcontent {
-  display: none;
-  padding: 6px 12px;
-  border: 0px solid #ccc;
-  border-top: none;
-  height:100%;
-
-}
-.list-group-item {
-    position: relative;
-    display: inline;
-    padding: .75rem 1.25rem;
-    margin-bottom: -1px;
-    background-color: #fff;
-    border: 1px solid rgba(0,0,0,.125);
-
-
-</style>
- </head>
- <body background="imagini/onpic1.jpg" style="text-align: center">
- 
-
- <div class="header">
- <h1><center><i>Olimpiada națională</i></center></h1>
- </div>
-<div class="tab" style="text-align: center;" align="center">
-  <a href="login.html">
-  <button class="tablinks" onclick="opentab(event, 'Candidati')">Logare</button>
-  </a>
-   <a href="registrationcomisie.html">
-  <button class="tablinks" onclick="opentab(event, 'Candidati')">Inregistrare</button>
-  </a>
-  <a href="home-candidati.php">
-  <button class="tablinks" onclick="opentab(event, 'Candidati')">Candidați</button>
-  </a>
-  <a href="home-subiecte.php">
-  <button class="tablinks" onclick="opentab(event, 'Subiecte')">Subiecte</button>
-  </a>
-  <a href="home-rezultate.php">
-  <button class="tablinks" onclick="opentab(event, 'Rezultate')">Rezultate</button>
-  </a>
-  <a href="contact.html">
-  <button class="tablinks" onclick="opentab(event, 'Contact')">Contact</button>
-  </a>
+<div class="row">
+	<div class="col-md-3"></div>
+	<div class="col-md-3">
+		<div name="subiecte" style="background-color:#e6e6e6; height:250px; text-align:center; background:url('imagini/cardback.jpg');  background-size: 100% auto;">
+			<a href="home-subiecte.php">
+				<button type="button" class="btn btn-info" style="margin-top:110px">Subiecte</button>
+			</a>
+		</div>
+	</div>
+	<div class="col-md-3">
+		<div name="rezultate" style="background-color:#e6e6e6; height:250px; text-align:center; background:url('imagini/rezultate.jpg');  background-size: 100% auto;">
+			<a href="home-rezultate.php">
+				<button type="button" class="btn btn-info" style="margin-top:110px">Rezultate</button>
+			</a>
+		</div>
+	</div>
 </div>
 
-
-<div name="home" style="margin-top: 50px">
-<center> <strong><h2>Organizatori Oficiali</h2></strong>
- <div class="g-heading-v9 text-center g-mb-30">
-							<!--<h2><strong>Organizatori</strong></h2>-->
-							<a class="logo inline">
-								<div class="text-center" style="opacity:.8;">
-									<img src="imagini/1.png" alt="" style="max-height: 100px; max-width:90%;">
-									<img src="imagini/2.png" alt="" style="max-height: 100px; max-width:90%;">
-									<img src="imagini/3.png" alt="" style="max-height: 100px; max-width:90%;">
-									<img src="imagini/4.png" alt="" style="max-height: 100px; max-width:90%;"></div>	</a></div></center><br><br>
-<div class="row">
-<div class="col-md-2"></div>
-<div class="col-md-8">
+<br>
+<div class="container" style="background-color:#e6e6e6; height:1300px; width:900px">
 <center>
+<br><br>
 <h3>Despre Olimpiade</h3>						
 <p>Olimpiadele și concursurile sunt competiții școlare pe discipline de studiu/domenii de pregătire, interdisciplinare și transdisciplinare, care au ca obiectiv general stimularea elevilor cu performanțe școlare înalte sau care au interes și aptitudini deosebite în domeniul științific, tehnico-aplicativ, cultural-artistic, civic şi în cel sportiv. Olimpiadele şi concursurile școlare promovează valorile culturale și etice fundamentale, spiritul de fair-play, competitivitatea și comunicarea interpersonală. Indiferent de domeniul lor sau de premiul oferit, aceste competiţii şcolare stimulează creativitatea şi gândirea critică, oferă motivația atât de necesară în procesul de învățare și ajută la identificarea și dezvoltarea talentelor, abilităților și cunoștințelor, contribuind la dezvoltarea personală și profesională a elevilor. </p>
 <p>Participarea la competițiile școlare este deschisă tuturor elevilor. Fiecare persoană are dreptul să primească o educaţie care dezvoltă abilitățile sale la întregul său potenţial. Garantarea acestui drept implică asigurarea egalităţii de șanse, oferind pentru fiecare persoană ajutor şi resurse în funcție de caracteristicile şi nevoile individuale. Identificarea şi susţinerea copiilor şi tinerilor capabili de performanţe şcolare înalte reprezintă o parte integrantă a politicilor educaţionale promovate de Ministerul Educaţiei.</p>
-<p>Participarea la competițiile școlare este deschisă tuturor elevilor. Fiecare persoană are dreptul să primească o educaţie care dezvoltă abilitățile sale la întregul său potenţial. Garantarea acestui drept implică asigurarea egalităţii de șanse, oferind pentru fiecare persoană ajutor şi resurse în funcție de caracteristicile şi nevoile individuale. Identificarea şi susţinerea copiilor şi tinerilor capabili de performanţe şcolare înalte reprezintă o parte integrantă a politicilor educaţionale promovate de Ministerul Educaţiei.</p>
+<br><br>	
 <h3>Locatii de concurs</h3>
 <div class="mapouter"><div class="gmap_canvas"><iframe width="848" height="765" id="gmap_canvas" src="https://maps.google.com/maps?q=Romania&t=&z=7&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div><style>.mapouter{position:relative;text-align:right;height:765px;width:848px;}.gmap_canvas {overflow:hidden;background:none!important;height:765px;width:848px;}</style></div>
-</div>
-</div>
-</div>
-
 </center>
+</div>
 
+<div class="container" style="background-color:#0E3667; height:330px; width:900px; color:white">
+<center>
+<br><br>
+Contact:
+<br><br>
+Adresa: Strada Domneasca, numarul 150, Galati, Romania
+<br>
+Telefon: 074 800 8008
+<br>
+Email: olimpiadanationala@info.com
+<br><br>
+Social media:
+<br>
+<a href="#" class="fa fa-facebook"></a>
+<a href="#" class="fa fa-twitter"></a>
+<a href="#" class="fa fa-instagram"></a>
+</center>
+<a href="Login.html" style="float:right; color:white">Login</a><br>
+<a href="registrationcomisie.html" style="float:right; color:white">Inregistrare</a>
+</div>
+
+
+<p id="demo"></p>
+	<script>
+	function myFunction() {
+	  var codunic = prompt("Te rugam sa introduci codul unic.");
+	  if (codunic === '123') {
+		  window.location.href='registrationcomisie.html';
+	  }
+	  else {
+		  alert("Cod unic gresit!");
+	  }
+	  document.getElementById("demo").innerHTML = text;
+	}
+	</script>
+	
 <script>
 function opentab(evt, cityName) {
   var i, tabcontent, tablinks;
@@ -185,8 +165,10 @@ function opentab(evt, cityName) {
   evt.currentTarget.className += " active";
 }
 </script>
- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
- </body>
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
+</body>
 </html>
